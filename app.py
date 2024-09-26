2a3aa24ab7442f1b9c611c47c871ab7e32e9fd5d
tej-as-kamble <tejaskk13@gmail.com>
2024-09-26 19:54:10 +0530

    added all needed route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect
 import pandas as pd
 
 app = Flask(__name__)
@@ -6,46 +6,64 @@ app = Flask(__name__)
 # Load data from CSV
 data = pd.read_csv('dataset/data.csv')
 
+tracks = []
+for index, row in data.iterrows():
+    track = {
+        'trackId': row['Track_ID'],
+        'mainArtistId': row['Main_Artist_ID'],
+        'trackImage': row['Track_Image'],
+        'songName': row['Song_Name'],
+        'artists': row['Artists'],
+        'album': row['Album/Movie_Name'],
+        'releaseYear': row['Release_Year'],
+        'trackUrl': row['Track_URL'],
+        'previewUrl': row['Preview_URL'],
+        'popularity': row['Popularity']
+    }
+    tracks.append(track)
+
 def get_tracks():
-    tracks = []
-    for index, row in data.iterrows():
-        track = {
-            'trackId': row['Track_ID'],
-            'mainArtistId': row['Main_Artist_ID'],
-            'trackImage': row['Track_Image'],
-            'songName': row['Song_Name'],
-            'artists': row['Artists'],
-            'album': row['Album/Movie_Name'],
-            'releaseYear': row['Release_Year'],
-            'trackUrl': row['Track_URL'],
-            'previewUrl': row['Preview_URL'],
-            'popularity': row['Popularity']
-        }
-        tracks.append(track)
-    # return sorted(tracks, key=lambda x: x['releaseYear'], reverse=True)
     return tracks
 
 
 @app.route('/')
-def index():
+@app.route('/<path:anything>')
+def red_track(anything=None):
+    return render_template('index.html')
+
+@app.route('/track/<trackId>')
+def track(trackId):
     return render_template('index.html')
 
 
-@app.route('/tracks/new-songs')
+@app.route('/hidden-url/tracks/new-songs')
 def new_songs():
     alltracks = get_tracks()
     return jsonify(sorted(alltracks, key=lambda x: x['releaseYear'], reverse=True))
 
-@app.route('/tracks/old-songs')
+@app.route('/hidden-url/tracks/old-songs')
 def old_songs():
     alltracks = get_tracks()
     return jsonify(sorted(alltracks, key=lambda x: x['releaseYear']))
 
 
-@app.route('/tracks/popular-songs')
+@app.route('/hidden-url/tracks/popular-songs')
 def popular_songs():
     alltracks = get_tracks()
     return jsonify(sorted(alltracks, key=lambda x: x['popularity'], reverse=True))
+
+
+@app.route('/hidden-url/track/<trackId>')
+def music_player(trackId):
+    alltracks = get_tracks()
+    
+    track = next((t for t in alltracks if t['trackId'] == trackId), None)
+    
+    if track:
+        return jsonify(track)
+    else:
+        return jsonify({"error": "Track not found"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,14 +1,15 @@
 const trackList = document.getElementById('track-list');
 const audioPlayer = document.getElementById('audio-player');
 const audioSource = document.getElementById('audio-source');
+const recommendedSongs = document.getElementById('recommended-songs');
 
 
 function createTrackTable(tracks, headingText) {
-    trackList.innerHTML = '';
+    const parts = document.createDocumentFragment();
 
     const heading = document.createElement('h2');
     heading.innerText = headingText;
-    trackList.appendChild(heading);
+    parts.appendChild(heading);
 
     const table = document.createElement('table');
     table.classList.add('track-table');
@@ -17,6 +18,7 @@ function createTrackTable(tracks, headingText) {
 
     tracks.forEach(track => {
         const row = document.createElement('tr');
+        row.id = track.trackId;
 
         // Image column
         const imgCell = document.createElement('td');
@@ -52,37 +54,68 @@ function createTrackTable(tracks, headingText) {
     });
 
     table.appendChild(tbody);
-    trackList.appendChild(table);
+    parts.appendChild(table);
+
+    trackList.innerHTML = '';
+    trackList.appendChild(parts);
+
+    document.querySelectorAll('tr').forEach(row => {
+        row.addEventListener('click', function() {
+            const newUrl = `/track/${row.id}`;
+            history.pushState(null, '', newUrl);
+            // console.log(window.location.pathname);
+
+            const buttons = document.querySelectorAll('.library-content-btn');
+    
+            buttons.forEach(btn => {
+                btn.classList.remove('active-btn');
+            });
+            createMusicPlayer();
+
+        });
+    });
+    
 }
 
-function setActive(button) {
+
+function setActive(btn) {
+    // console.log(btn);
     const buttons = document.querySelectorAll('.library-content-btn');
     
     buttons.forEach(btn => {
         btn.classList.remove('active-btn');
     });
     
-    button.classList.add('active-btn');
+    btn.classList.add('active-btn');
 }
+
 
 function newRelease() {
     console.log("newRelease working");
-    const newReleaseBtn = document.getElementById('new-releases-btn');
-    newReleaseBtn.classList.add('active-btn');
-    fetch('/tracks/new-songs')
+    const newUrl = `/new-release`;
+    history.pushState(null, '', newUrl);
+
+    const curr_Btn = document.getElementById("new-releases-btn");
+    setActive(curr_Btn);
+
+    fetch('/hidden-url/tracks/new-songs')
         .then(response => response.json())
         .then(tracks => {
             createTrackTable(tracks, 'New Release');
         })
         .catch(error => console.error('Error fetching tracks:', error));
 }
-newRelease();
 
 
 function topSongs() {
     console.log("topSongs working");
+    const newUrl = `/top-songs`;
+    history.pushState(null, '', newUrl);
     
-    fetch('/tracks/popular-songs')
+    const curr_Btn = document.getElementById("top-songs-btn");
+    setActive(curr_Btn);
+
+    fetch('/hidden-url/tracks/popular-songs')
         .then(response => response.json())
         .then(tracks => {
             createTrackTable(tracks, 'Popular Songs');
@@ -90,11 +123,30 @@ function topSongs() {
         .catch(error => console.error('Error fetching tracks:', error));
 }
 
+function bestArtists(){
+    console.log("bestArtists working");
+    trackList.innerHTML = '';
+    const newUrl = `/best-artists`;
+    history.pushState(null, '', newUrl);
+
+    const curr_Btn = document.getElementById("best-artists-btn");
+    setActive(curr_Btn);
+
+    const heading = document.createElement('h2');
+    heading.innerText = "We will add BEST Artists soon...";
+    trackList.appendChild(heading);
+}
+
 
 function newSongs() {
     console.log("newRelease working");
+    const newUrl = `/new-songs`;
+    history.pushState(null, '', newUrl);
 
-    fetch('/tracks/new-songs')
+    const curr_Btn = document.getElementById("new-songs-btn");
+    setActive(curr_Btn);
+
+    fetch('/hidden-url/tracks/new-songs')
         .then(response => response.json())
         .then(tracks => {
             createTrackTable(tracks, 'New Songs');
@@ -104,11 +156,48 @@ function newSongs() {
 
 function oldSongs(){
     console.log("topSongs working");
+    const newUrl = `/old-songs`;
+    history.pushState(null, '', newUrl);
+
+    const curr_Btn = document.getElementById("old-songs-btn");
+    setActive(curr_Btn);
     
-    fetch('/tracks/old-songs')
+    fetch('/hidden-url/tracks/old-songs')
         .then(response => response.json())
         .then(tracks => {
             createTrackTable(tracks, 'Old Songs');
         })
         .catch(error => console.error('Error fetching tracks:', error));
+}
+
+
+const url_pathname = window.location.pathname.split('/');
+console.log("url_pathname =", url_pathname[1]);
+if(url_pathname[1]==="top-songs"){
+    topSongs();
+}
+else if(url_pathname[1]==="best-artists"){
+    bestArtists();
+}
+else if(url_pathname[1]==="new-songs"){
+    newSongs();
+}
+else if(url_pathname[1]==="old-songs"){
+    oldSongs();
+}
+else if(url_pathname[1]==="track" && url_pathname[2]){
+    // console.log(url_pathname[2]);
+    createMusicPlayer();
+}
+else{
+    newRelease();
+}
+
+
+function createMusicPlayer(){
+    console.log("music player working");
+    trackList.innerHTML = '';
+    const heading = document.createElement('h2');
+    heading.innerText = "We will add Music Player soon...";
+    trackList.appendChild(heading);
 }
